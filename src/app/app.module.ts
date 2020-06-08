1d4ad7ceac40e26929450e3990dfc12abdfc6fcf
mhrxadev <60345056+mhrxadev@users.noreply.github.com>
2020-06-08 18:42:19 +0430

    sweetAlert

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {AppRoutingModule, routingComponents} from './app-routing.module';
-
 import { AppComponent } from './app.component';
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {EshopInterceptor} from "./Utilities/EshopInterceptor";
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
 
 
 
@@ -19,7 +19,8 @@ import {EshopInterceptor} from "./Utilities/EshopInterceptor";
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    [SweetAlert2Module.forRoot()]
   ],
   providers: [
     {
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {RegisterUserDTO} from "../DTOs/Account/RegisterUserDTO";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+
+  registerUser(registerData: RegisterUserDTO): Observable<any> {
+    return this.http.post<any>('/account/register', registerData);
+  }
+
+
 }
